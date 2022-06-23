272e7a468182febf0ddb2631cfd2bf84fd7259eb
panosjapan7 <panosjapan@gmail.com>
2022-06-23 14:00:09 +0200

    Disbale button if message is empty or <10 chars

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,24 @@ import Button from "../shared/Button";
 
 function FeedbackForm() {
     const [text, setText] = useState("");
+    const [btnDisabled, setBtnDisable] = useState(true);
+    const [message, setMessage] = useState("");
     
     const handleTextChange = (e) => {
+        
+        if (text === "") {
+            setBtnDisable(true);
+            setMessage(null);
+        }
+        else if (text.trim().length < 10) {
+            setMessage("Text must be at least 10 characters");
+            setBtnDisable(true);
+        }
+        else {
+            setMessage(null);
+            setBtnDisable(false);
+        }
+        
         setText(e.target.value);
     }
 
@@ -20,8 +36,9 @@ function FeedbackForm() {
                     onChange={handleTextChange} 
                     value={text} 
                 />
-                <Button type="submit">Send</Button>
+                <Button type="submit" isDisabled={btnDisabled}>Send</Button>
             </div>
+            {message && <div className="message">{message}</div>}
         </form>
     </Card>
     )
