ebdf85204e5f21d7afc5d77bdfba2168ab0405ab
panosjapan7 <panosjapan@gmail.com>
2022-06-24 11:21:03 +0200

    concurrently package, local db JSON

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,30 +1,26 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import {v4 as uuidv4} from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState([{
-        id: 1,
-        text: "This feedback item 1",
-        rating: 10,
-    },
-    {
-        id: 2,
-        text: "This feedback item 2",
-        rating: 9,
-    },
-    {
-        id: 3,
-        text: "This feedback item 3",
-        rating: 7,
-    },
-    ]);
-
+    const [isLoading, setIsLoading] = (true);
+    const [feedback, setFeedback] = useState([]);
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false,
     });
+
+    useEffect(() => {
+        fetchFeedback();
+    }, []);
+    
+    const fetchFeedback = async () => {
+        const response = await fetch("http://localhost:5000/feedback?_sort=id&_order=desc");
+        const data = await response.json();
+        setFeedback(data);
+        setIsLoading(false);
+    };
 
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4();
@@ -50,7 +46,8 @@ export const FeedbackProvider = ({ children }) => {
 
     return <FeedbackContext.Provider value={{
                 feedback, 
-                feedbackEdit, 
+                feedbackEdit,
+                isLoading,
                 deleteFeedback, 
                 addFeedback, 
                 editFeedback, 
